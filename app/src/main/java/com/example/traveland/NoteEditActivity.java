6fed8ff6ba2ed9604154c7849c79961edfb6cb6b
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-11-06 14:55:07 +0300

    opakommit

--- app/src/main/java/com/example/traveland/NoteEditActivity.java
+++ app/src/main/java/com/example/traveland/NoteEditActivity.java
@@ -1,8 +1,10 @@
 package com.example.traveland;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,9 @@ public class NoteEditActivity extends AppCompatActivity {                 //Вт
     private int position = -1;                                            //переменная position для хранения позиции заметки в списке
 
     private EditText titleEditText;                                       //заголовок заметки
-    private EditText contentEditText;                                     //содержимое заметки
+    private EditText contentEditText;
+    private Button backButton;
+    private Button saveButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +24,8 @@ public class NoteEditActivity extends AppCompatActivity {                 //Вт
 
         titleEditText = findViewById(R.id.titleEditText);
         contentEditText = findViewById(R.id.contentEditText);
+        backButton = findViewById(R.id.backbutton);
+        saveButton = findViewById(R.id.saveButton);
 
         Intent intent = getIntent();
         position = intent.getIntExtra("position", -1);
@@ -29,17 +35,27 @@ public class NoteEditActivity extends AppCompatActivity {                 //Вт
             titleEditText.setText(title);
             contentEditText.setText(content);
         }
-    }
 
-    public void onSaveNoteClick(View view) {                            //метод для кнопки Сохранить
-        String title = titleEditText.getText().toString();              //получает тему
-        String content = contentEditText.getText().toString();          //получает текст
 
-        Intent data = new Intent();                                     //создаёт intent в который помещает данные
-        data.putExtra("position", position);
-        data.putExtra("title", title);
-        data.putExtra("content", content);
-        setResult(RESULT_OK, data);
-        finish();                                                       //завершается активити
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {                          //метод для кнопки Сохранить
+                String title = titleEditText.getText().toString();              //получает тему
+                String content = contentEditText.getText().toString();          //получает текст
+
+                Intent data = new Intent();                                     //создаёт intent в который помещает данные
+                data.putExtra("position", position);
+                data.putExtra("title", title);
+                data.putExtra("content", content);
+                setResult(RESULT_OK, data);
+                finish();                                                      //завершается активити
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
