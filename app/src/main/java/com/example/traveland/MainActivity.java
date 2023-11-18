52e6bc62baa67dc0fd9d3ba4a823d45041d079a2
l-Mel-l <125726970+l-Mel-l@users.noreply.github.com>
2023-11-18 12:38:44 +0300

    чё-то сделал

--- app/src/main/java/com/example/traveland/MainActivity.java
+++ app/src/main/java/com/example/traveland/MainActivity.java
@@ -22,107 +22,107 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);}}
         //getSupportActionBar().hide();
 
-        dataBaseAccessor = new DataBaseAccessor(this);
-        listView = findViewById(R.id.listView); // Инициализируем переменную listView
-
-        // Получаем адаптер курсора из базы данных
-        cursorAdapter = dataBaseAccessor.getCursorAdapter(this, android.R.layout.simple_list_item_1,
-                new int[]{android.R.id.text1});
-        listView.setAdapter(cursorAdapter);
-
-        // Назначаем слушатель для кнопки добавления заметки
-        Button addNoteButton = findViewById(R.id.addNoteButton);
-        addNoteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Открываем вторую активити для редактирования заметки
-                Intent intent = new Intent(MainActivity.this, NoteEditActivity.class);
-                startActivityForResult(intent, 1);
-                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
-            }
-        });
-
-        // Назначаем слушатель для элементов списка
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Cursor cursor = (Cursor) parent.getItemAtPosition(position);
-                int noteId = cursor.getInt(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_ID));
-                String theme = cursor.getString(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_THEME));
-                String note = cursor.getString(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_NOTE));
-
-                // Обработка нажатия на элемент списка
-                Intent intent = new Intent(MainActivity.this, NoteEditActivity.class);
-                intent.putExtra("noteId", noteId); // Передаем noteId в Intent
-                intent.putExtra("theme", theme);
-                intent.putExtra("note", note);
-                startActivityForResult(intent, 1); // Используем startActivityForResult для получения результата от NoteEditActivity
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-            }
-        });
-        //для удаления при зажатии на заметку
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Cursor cursor = (Cursor) parent.getItemAtPosition(position);
-                int noteId = cursor.getInt(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_ID));
-                String theme = cursor.getString(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_THEME));
-
-                // Отображение диалогового окна для подтверждения удаления
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Удалить заметку")
-                        .setMessage("Вы уверены, что хотите удалить заметку с темой: " + theme + "?")
-                        .setPositiveButton("Удалить", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Удаление заметки из базы данных
-                                dataBaseAccessor.deleteNoteById(noteId);
-                                // Обновление списка заметок
-                                cursorAdapter.swapCursor(dataBaseAccessor.getAllNotes());
-                            }
-                        })
-                        .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Ничего не делать при отмене
-                            }
-                        })
-                        .show();
-
-                return true;
-            }
-        });
-    }
-
-    // Обработка результата из второй активити
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
-            int position = data.getIntExtra("position", -1);
-            String title = data.getStringExtra("title");
-            String content = data.getStringExtra("content");
-
-            if (position != -1) {
-                // Редактирование существующей заметки
-                dataBaseAccessor.updateNote(position, title, content);
-            } else {
-                // Создание новой заметки
-                dataBaseAccessor.insertNote(title, content);
-            }
-
-            // Обновляем курсор адаптера
-            cursorAdapter.swapCursor(dataBaseAccessor.getAllNotes());
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        cursorAdapter.getCursor().close();
-    }
-}
+//        dataBaseAccessor = new DataBaseAccessor(this);
+//        listView = findViewById(R.id.listView); // Инициализируем переменную listView
+//
+//        // Получаем адаптер курсора из базы данных
+//        cursorAdapter = dataBaseAccessor.getCursorAdapter(this, android.R.layout.simple_list_item_1,
+//                new int[]{android.R.id.text1});
+//        listView.setAdapter(cursorAdapter);
+//
+//        // Назначаем слушатель для кнопки добавления заметки
+//        Button addNoteButton = findViewById(R.id.addNoteButton);
+//        addNoteButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Открываем вторую активити для редактирования заметки
+//                Intent intent = new Intent(MainActivity.this, NoteEditActivity.class);
+//                startActivityForResult(intent, 1);
+//                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+//            }
+//        });
+//
+//        // Назначаем слушатель для элементов списка
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Cursor cursor = (Cursor) parent.getItemAtPosition(position);
+//                int noteId = cursor.getInt(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_ID));
+//                String theme = cursor.getString(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_THEME));
+//                String note = cursor.getString(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_NOTE));
+//
+//                // Обработка нажатия на элемент списка
+//                Intent intent = new Intent(MainActivity.this, NoteEditActivity.class);
+//                intent.putExtra("noteId", noteId); // Передаем noteId в Intent
+//                intent.putExtra("theme", theme);
+//                intent.putExtra("note", note);
+//                startActivityForResult(intent, 1); // Используем startActivityForResult для получения результата от NoteEditActivity
+//                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+//            }
+//        });
+//        //для удаления при зажатии на заметку
+//        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+//                Cursor cursor = (Cursor) parent.getItemAtPosition(position);
+//                int noteId = cursor.getInt(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_ID));
+//                String theme = cursor.getString(cursor.getColumnIndexOrThrow(DataBaseAccessor.COLUMN_THEME));
+//
+//                // Отображение диалогового окна для подтверждения удаления
+//                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+//                builder.setTitle("Удалить заметку")
+//                        .setMessage("Вы уверены, что хотите удалить заметку с темой: " + theme + "?")
+//                        .setPositiveButton("Удалить", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                // Удаление заметки из базы данных
+//                                dataBaseAccessor.deleteNoteById(noteId);
+//                                // Обновление списка заметок
+//                                cursorAdapter.swapCursor(dataBaseAccessor.getAllNotes());
+//                            }
+//                        })
+//                        .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                // Ничего не делать при отмене
+//                            }
+//                        })
+//                        .show();
+//
+//                return true;
+//            }
+//        });
+//    }
+//
+//    // Обработка результата из второй активити
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+//            int position = data.getIntExtra("position", -1);
+//            String title = data.getStringExtra("title");
+//            String content = data.getStringExtra("content");
+//
+//            if (position != -1) {
+//                // Редактирование существующей заметки
+//                dataBaseAccessor.updateNote(position, title, content);
+//            } else {
+//                // Создание новой заметки
+//                dataBaseAccessor.insertNote(title, content);
+//            }
+//
+//            // Обновляем курсор адаптера
+//            cursorAdapter.swapCursor(dataBaseAccessor.getAllNotes());
+//        }
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        cursorAdapter.getCursor().close();
+//    }
+//}
